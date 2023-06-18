c88b25d529aafd2c653cb2da230543ed6c4d3156
Nick Raziborsky <werkamsus@googlemail.com>
2023-06-18 20:12:18 +0200

    config: change site title

--- components/HeroDescription.tsx
+++ components/HeroDescription.tsx
@@ -1,6 +1,6 @@
 export function HeroDescription() {
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+    <section className="container grid items-center gap-6 p-4 pb-8 pt-6 md:py-10 text-center justify-center">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           🦎 Magic File Converter
