fd4a16a84c83c622e7f11d1d1dc60ab04da555aa
Nick Raziborsky <werkamsus@googlemail.com>
2023-06-18 16:10:46 +0200

    feat: gif generation

--- components/FFmpegComponent.tsx
+++ components/FFmpegComponent.tsx
@@ -1,8 +1,11 @@
 "use client"
 
 import { ChangeEvent, useEffect, useState } from "react"
-import { createFFmpeg } from "@ffmpeg/ffmpeg"
+import Image from "next/image"
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg"
+import { Loader2 } from "lucide-react"
 
+import { Button } from "./ui/button"
 import { Input } from "./ui/input"
 
 const ffmpeg = createFFmpeg({
@@ -13,7 +16,7 @@ const ffmpeg = createFFmpeg({
 export default function FFmpegComponent() {
   const [ready, setReady] = useState(false)
   const [video, setVideo] = useState<File>()
-  const [gif, setGif] = useState()
+  const [gif, setGif] = useState<string>()
 
   const load = async () => {
     if (!ffmpeg.isLoaded()) {
@@ -40,12 +43,54 @@ export default function FFmpegComponent() {
     }
   }
 
+  const convertToGif = async (): Promise<void> => {
+    if (!video) return
+
+    ffmpeg.isLoaded() || (await ffmpeg.load())
+    ffmpeg.FS("writeFile", "test.mp4", await fetchFile(video))
+
+    //detect uploaded file.
+    const splitArgs = "-i test.mp4 -ss 5 -t 5 -acodec libmp3lame output".split(
+      " "
+    )
+    await ffmpeg.run(...splitArgs)
+
+    // await ffmpeg.run(
+    //   "-i",
+    //   "test.mp4",
+    //   "-t",
+    //   "2.5",
+    //   "-ss",
+    //   "2.0",
+    //   "-f",
+    //   "gif",
+    //   "out.gif"
+    // )
+
+    const data = ffmpeg.FS("readFile", "output")
+
+    
+    const url = URL.createObjectURL(
+      new Blob([data.buffer], { type: "audio/mp3" })
+    )
+    setGif(url)
+  }
   return (
-    <div>
-      <h1>Loading FFMPEG {ready && <p>Ready!</p>}</h1>
+    <div className="flex max-w-[600px] flex-col gap-2">
+      {ready ? (
+        <p>Ready!</p>
+      ) : (
+        <p>
+          <Loader2 className="h-4 w-4 animate-spin" />
+          Loading...
+        </p>
+      )}
       <Input type="file" onChange={handleVideoChange} />
 
+      <Button onClick={convertToGif}>Convert to gif</Button>
       {video && <video controls width="250" src={URL.createObjectURL(video)} />}
+      {gif && <Image src={gif} width={200} height={200} alt="Converted GIF" />}
+      {video && <video controls width="250" src={gif} />}
     </div>
   )
 }
